aa82d005b234ea1420a604d48e2026cbf1f7a561
Gabriel Sagot <sagotgab@gmail.com>
2026-09-21 14:43:15 +0000

    feat: ✨ add useEffect hook to fetch popular movies from api

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react'
+
 export default function App() {
+  useEffect(() => {
+    // fetch data from an API /api/movies/popular
+    fetch('/api/movies/popular')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }, [])
+
   return (
     <div>
       Hello Vite from React!
