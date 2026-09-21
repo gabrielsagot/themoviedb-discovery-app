fad68826a2500b87f76e4c0b398abeda527a5ec8
Gabriel Sagot <sagotgab@gmail.com>
2026-09-21 16:00:38 +0000

    feat: ✨ forward language, page and region query params to tmdb

--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { tmdbAccessToken } from './config';
+import { DEFAULT_LANGUAGE, DEFAULT_PAGE, DEFAULT_REGION } from './constants';
 import type {
   TmdbMoviesRawResponse,
   MoviesApiResponse,
@@ -23,8 +24,18 @@ app.get(
   '/api/movies/popular',
   async (_req: express.Request, res: express.Response) => {
     try {
+      // Create a URLSearchParams object to build the query string for the TMDB API request
+      const queryParams = new URLSearchParams();
+
+      // Extract query parameters from the request and append them to the query string
+      const { language, page, region } = _req.query;
+
+      queryParams.append('language', (language as string) || DEFAULT_LANGUAGE);
+      queryParams.append('page', (page as string) || DEFAULT_PAGE);
+      queryParams.append('region', (region as string) || DEFAULT_REGION);
+
       const response = await fetch(
-        'https://api.themoviedb.org/3/movie/popular',
+        `https://api.themoviedb.org/3/movie/popular?${queryParams.toString()}`,
         {
           headers: {
             Authorization: `Bearer ${tmdbAccessToken}`,
